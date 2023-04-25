4b3d76f5bcf606fec6cdb2982c46f9fd842a5a50
Steven Angel <stevenangel.ing@gmail.com>
2023-04-24 19:55:57 -0500

    Corrección en revalidarToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,7 @@ const crearUsuario = async( req, res = response ) => {
         if ( usuario ) {
             return res.status(400).json({
                 ok: false,
-                msg: 'Un usuario existe con ese correo'
+                msg: 'El correo ya se encuentra registrado'
             })
         }
         
@@ -110,6 +110,8 @@ const revalidarToken = async(req, res = response) => {
 
     res.json({
         ok:true,
+        uid,
+        name,
         token
     })
 }
